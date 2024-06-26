60bba83b3ed6e336b00339c607f9b0a39768b601
cisco <cisco@UPOAULA10307>
2024-06-26 10:48:58 -0500

    Agregar archivos de proyecto.

---
+++ JulianaSosaAPIPublica/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JulianaSosaAPIPublica
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ JulianaSosaAPIPublica/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JulianaSosaAPIPublica
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
